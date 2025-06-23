2441755cad6bd4d2ef1c9471e24e7bfbadcd0b40
zjm54321 <changchiaming@qq.com>
2025-06-23 11:46:51 +0800

    feat(sog3): firmware related update
    更新了surface go3  背光，音频与蓝牙相关功能

--- host/sgo3/default.nix
+++ host/sgo3/default.nix
@@ -1,18 +1,51 @@
 {
+  pkgs,
+  ...
+}:
+{
   imports = [
-    # Include the results of the hardware scan.
     ./hardware-configuration.nix
   ];
 
-  # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "sgo3"; # Define your hostname.
+  networking.hostName = "sgo3";
   networking.networkmanager.enable = true;
-  networking.proxy.mihomo.enable = true; # 启用代理
 
-  system.stateVersion = "25.05"; # Did you read the comment?
+  environment.systemPackages = with pkgs; [
+    #============================= 背光 =================================
+    brightnessctl
+
+    #============================= 音频(PipeWire) =======================
+    pulseaudio # 提供 `pactl` 命令，某些应用程序需要此命令（例如 sonic-pi）
+  ];
+
+  # PipeWire 是一个新的低级多媒体框架。
+  # 它旨在以最小延迟提供音频和视频的捕获和播放功能。
+  # 它支持基于 PulseAudio、JACK、ALSA 和 GStreamer 的应用程序。
+  # PipeWire 具有出色的蓝牙支持，可以作为 PulseAudio 的良好替代品。
+  #     https://nixos.wiki/wiki/PipeWire
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # 如果您想使用 JACK 应用程序，请取消注释此行
+    jack.enable = true;
+    wireplumber.enable = true;
+  };
+  # rtkit 是可选的，但建议启用
+  security.rtkit.enable = true;
+  # 禁用 pulseaudio，因为它与 pipewire 冲突。
+  services.pulseaudio.enable = false;
+
+  #============================= 蓝牙 =============================
+  hardware.bluetooth.enable = true;
+  services.blueman.enable = true;
+
+  system.stateVersion = "25.05";
 
   services.display.desktop = "niri";
+  networking.proxy.mihomo.enable = true; # 启用代理
 }
