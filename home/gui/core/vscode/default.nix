8f8e146485becddc7c42319eadaca9ffb1e37d2d
zjm54321 <1779036876@qq.com>
2025-06-25 23:26:52 +0800

    fix(vscode): ime input

--- home/gui/core/vscode/default.nix
+++ home/gui/core/vscode/default.nix
@@ -5,13 +5,20 @@
 }:
 {
 
-  home.packages = with pkgs; [
-    vscodium
-  ];
-
   programs.vscode = {
     enable = true;
-    package = pkgs.vscodium;
+    package = pkgs.vscodium.override {
+      commandLineArgs = [
+        "--ozone-platform-hint=auto"
+        "--ozone-platform=wayland"
+        # 使其在Gtk4下使用GTK_IM_MODULE，这样fcitx5就能正常工作
+        # (目前只有chromium/chrome支持，electron还不支持)
+        "--gtk-version=4"
+        # 使用text-input-v3
+        "--enable-wayland-ime"
+        "--wayland-text-input-version=3"
+      ];
+    };
     profiles.default.extensions = import ./extensions.nix {
       inherit nix-vscode-extensions pkgs;
     };
