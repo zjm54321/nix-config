9f7e15bdc9097ad19f3ee72b1d8a2e77f1c2a3c5
zjm54321 <changchiaming@qq.com>
2025-06-27 23:11:09 +0800

    refactor(niri): config with flake
    初步完成了 #5 的配置，目前仅剩部分 workspace 相关内容尚未迁移。

--- home/gui/niri/default.nix
+++ home/gui/niri/default.nix
@@ -3,6 +3,10 @@
   ...
 }:
 {
+  imports = [
+    ./keybindings.nix
+  ];
+
   home.packages = with pkgs; [
     anyrun # 应用程序启动器
     libnotify # 提供 notify-send 命令
@@ -21,6 +25,78 @@
   # 通知设置
   services.mako.enable = true;
 
-  programs.niri.config = builtins.readFile ./niri.kdl;
+  programs.niri.settings = {
+    environment = {
+      DISPLAY = ":0";
+    };
+
+    spawn-at-startup = [
+      {
+        command = [
+          "systemctl"
+          "--user"
+          "restart"
+          "waybar.service"
+        ];
+      }
+      { command = [ "fcitx5" ]; }
+      { command = [ "xwayland-satellite" ]; }
+      { command = [ "wezterm" ]; }
+    ];
+
+    # https://github.com/YaLTeR/niri/wiki/Configuration:-Input
+    input.keyboard.numlock = true;
+    # https://github.com/YaLTeR/niri/wiki/Configuration:-Layout
+    layout = {
+      gaps = 12;
+      center-focused-column = "on-overflow";
+
+      preset-column-widths = [
+        { proportion = 1. / 3.; }
+        { proportion = 1. / 2.; }
+        { proportion = 2. / 3.; }
+      ];
+      preset-window-heights = [
+        { proportion = 1. / 3.; }
+        { proportion = 1. / 2.; }
+        { proportion = 2. / 3.; }
+      ];
+      default-column-width.proportion = 1. / 2.;
+
+      focus-ring = {
+        enable = true;
+        width = 2;
+        active.color = "#7fc8ff";
+        inactive.color = "#505050";
+      };
+    };
+
+    prefer-no-csd = true;
+
+    window-rules = [
+      {
+        clip-to-geometry = true;
+        geometry-corner-radius = {
+          top-left = 16.0;
+          top-right = 16.0;
+          bottom-left = 16.0;
+          bottom-right = 16.0;
+        };
+      }
+      {
+        matches = [ { app-id = "^org\.wezfurlong\.wezterm$"; } ];
+        default-column-width = { };
+      }
+      {
+        matches = [
+          {
+            app-id = "firefox$";
+            title = "^Picture-in-Picture$";
+          }
+        ];
+        open-floating = true;
+      }
+    ];
+  };
 
 }
---
+++ home/gui/niri/keybindings.nix
@@ -0,0 +1,172 @@
+{
+  config,
+  ...
+}:
+{
+  programs.niri.settings.binds = with config.lib.niri.actions; {
+    # niri 键盘快捷键
+    "Mod+Shift+Slash" = {
+      hotkey-overlay.title = "快捷键展示";
+      action = show-hotkey-overlay;
+    };
+    "Mod+Shift+E" = {
+      hotkey-overlay.title = "退出 Niri";
+      action = quit;
+    };
+    "Ctrl+Alt+Delete".action = quit;
+    "Mod+O" = {
+      hotkey-overlay.title = "打开预览";
+      repeat = false;
+      action = toggle-overview;
+    };
+
+    # 打开应用程序
+    "Mod+Return" = {
+      hotkey-overlay.title = "打开终端: wezterm";
+      action = spawn "wezterm";
+    };
+    "Mod+Space" = {
+      hotkey-overlay.title = "运行应用程序: anyrun";
+      action = spawn "anyrun";
+    };
+    "Mod+Alt+L" = {
+      hotkey-overlay.title = "锁定屏幕: hyprlock";
+      action = spawn "hyprlock";
+    };
+
+    # 多媒体按键
+    "XF86AudioRaiseVolume" = {
+      allow-when-locked = true;
+      action = spawn "wpctl" "set-volume" "@DEFAULT_AUDIO_SINK@" "5%+";
+    };
+    "XF86AudioLowerVolume" = {
+      allow-when-locked = true;
+      action = spawn "wpctl" "set-volume" "@DEFAULT_AUDIO_SINK@" "5%-";
+    };
+    "XF86AudioMute" = {
+      allow-when-locked = true;
+      action = spawn "wpctl" "set-mute" "@DEFAULT_AUDIO_SINK@" "toggle";
+    };
+    "XF86AudioMicMute" = {
+      allow-when-locked = true;
+      action = spawn "wpctl" "set-mute" "@DEFAULT_AUDIO_SOURCE@" "toggle";
+    };
+    "XF86MonBrightnessUp" = {
+      allow-when-locked = true;
+      action = spawn "brightnessctl" "set" "5%+";
+    };
+    "XF86MonBrightnessDown" = {
+      allow-when-locked = true;
+      action = spawn "brightnessctl" "set" "5%-";
+    };
+    "Print" = {
+      hotkey-overlay.title = "截图";
+      action = screenshot;
+    };
+
+    # 窗口控制
+    "Mod+H" = {
+      hotkey-overlay.title = "焦点左移";
+      action = focus-column-left;
+    };
+    "Mod+J" = {
+      hotkey-overlay.title = "焦点下移";
+      action = focus-window-down;
+    };
+    "Mod+K" = {
+      hotkey-overlay.title = "焦点上移";
+      action = focus-window-up;
+    };
+    "Mod+L" = {
+      hotkey-overlay.title = "焦点右移";
+      action = focus-column-right;
+    };
+
+    "Mod+Ctrl+H" = {
+      hotkey-overlay.title = "移动列至左侧";
+      action = move-column-left;
+    };
+    "Mod+Ctrl+J" = {
+      hotkey-overlay.title = "移动窗口至下方";
+      action = move-window-down;
+    };
+    "Mod+Ctrl+K" = {
+      hotkey-overlay.title = "移动窗口至上方";
+      action = move-window-up;
+    };
+    "Mod+Ctrl+L" = {
+      hotkey-overlay.title = "移动列至右侧";
+      action = move-column-right;
+    };
+    "Mod+Left".action = focus-column-left;
+    "Mod+Down".action = focus-window-down;
+    "Mod+Up".action = focus-window-up;
+    "Mod+Right".action = focus-column-right;
+    "Mod+Ctrl+Left".action = move-column-left;
+    "Mod+Ctrl+Down".action = move-window-down;
+    "Mod+Ctrl+Up".action = move-window-up;
+    "Mod+Ctrl+Right".action = move-column-right;
+
+    "Mod+Q" = {
+      hotkey-overlay.title = "关闭窗口";
+      action = close-window;
+    };
+    "Mod+BracketLeft" = {
+      hotkey-overlay.title = "向左压入或弹出窗口";
+      action = consume-or-expel-window-left;
+    };
+    "Mod+BracketRight" = {
+      hotkey-overlay.title = "向右压入或弹出窗口";
+      action = consume-or-expel-window-right;
+    };
+    "Mod+Comma".action = consume-window-into-column;
+    "Mod+Period".action = expel-window-from-column;
+    "Mod+R" = {
+      hotkey-overlay.title = "切换列宽";
+      action = switch-preset-column-width;
+    };
+    "Mod+Shift+R" = {
+      hotkey-overlay.title = "切换列高";
+      action = switch-preset-window-height;
+    };
+    "Mod+Ctrl+R".action = reset-window-height;
+    "Mod+F" = {
+      hotkey-overlay.title = "最大化";
+      action = maximize-column;
+    };
+    "Mod+Shift+F".action = fullscreen-window;
+    "Mod+Ctrl+F".action = expand-column-to-available-width;
+    "F11".action = fullscreen-window;
+
+    "Mod+C".action = center-column;
+    "Mod+Ctrl+C".action = center-visible-columns;
+
+    "Mod+Minus".action = set-column-width "-10%";
+    "Mod+Equal".action = set-column-width "+10%";
+    "Mod+Shift+Minus".action = set-window-height "-10%";
+    "Mod+Shift+Equal".action = set-window-height "+10%";
+
+    "Mod+V" = {
+      hotkey-overlay.title = "切换窗口浮动模式";
+      action = toggle-window-floating;
+    };
+    "Mod+Shift+V" = {
+      hotkey-overlay.title = "浮动与平铺焦点切换";
+      action = switch-focus-between-floating-and-tiling;
+    };
+
+    "Mod+W".action = toggle-column-tabbed-display;
+
+    # 工作区
+    "Mod+1".action = focus-workspace 1;
+    "Mod+2".action = focus-workspace 2;
+    "Mod+3".action = focus-workspace 3;
+    "Mod+4".action = focus-workspace 4;
+    "Mod+5".action = focus-workspace 5;
+    "Mod+6".action = focus-workspace 6;
+    "Mod+7".action = focus-workspace 7;
+    "Mod+8".action = focus-workspace 8;
+    "Mod+9".action = focus-workspace 9;
+
+  };
+}
