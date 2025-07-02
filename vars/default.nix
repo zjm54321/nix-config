df9edcf65b3235284b526af20498afb41d7cfc8b
zjm54321 <changchiaming@qq.com>
2025-07-02 19:30:53 +0800

    feat(e2): add public key of e2

--- vars/default.nix
+++ vars/default.nix
@@ -6,5 +6,7 @@
   gpgAuthorizedKeys = [ "" ];
   sshAuthorizedKeys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICgt/2h5OROlOs6L9+SCvSihiq5jcS5TqA+pgeDU9ma5 ming@136kf"
+    # e2 TPM 密钥
+    "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBBiclJsws+2d6x1hslry7RAf5CkiP8kJNFOQIpiYHdJ2pVPWejC2ZpzlznDclQwwaju0mnG4GS8XgSK9nr0BPr0="
   ];
 }
