e0fdfea392f9bee774e2621ccc9c52042bc284eb
zjm54321 <changchiaming@qq.com>
2025-07-02 15:12:24 +0800

    fix(e2):delete unused hardware configuration

--- host/e2/hardware.nix
+++ host/e2/hardware.nix
@@ -3,7 +3,7 @@
   ...
 }:
 {
-  # Intel UHD Graphics 615
+  # Intel UHD Graphics 630
   hardware.graphics = {
     enable = true;
     extraPackages = with pkgs; [
@@ -20,9 +20,6 @@
   };
 
   environment.systemPackages = with pkgs; [
-    # 背光
-    brightnessctl
-
     # 音频(PipeWire)
     pulseaudio # 提供 `pactl` 命令，某些应用程序需要此命令（例如 sonic-pi）
   ];
@@ -46,10 +43,6 @@
   security.rtkit.enable = true;
   # 禁用 pulseaudio，因为它与 pipewire 冲突。
   services.pulseaudio.enable = false;
-
-  # 蓝牙
-  hardware.bluetooth.enable = true;
-  services.blueman.enable = true;
 
   # 打印机
   services.printing = {
