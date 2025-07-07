a6e1d29ca34b83b73d62a581e831f91c621874bf
zjm54321 <zhang_jia_ming@outlook.com>
2025-07-07 21:41:23 +0800

    feat(sgo3): enable tpm2 for sgo3

--- host/sgo3/default.nix
+++ host/sgo3/default.nix
@@ -12,6 +12,7 @@
 
   system.stateVersion = "25.05";
 
+  services.tpm2.enable = true;
   services.display.desktop = "niri";
   services.display.bootanmination.enable = true;
   networking.proxy.mihomo.enable = true; # 启用代理
