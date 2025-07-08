71b62ffc80e92f3d8c33a0a57dd0ec209f5d9e43
zjm54321 <zhang_jia_ming@outlook.com>
2025-07-08 12:56:49 +0800

    feat: add gpg keys

--- vars/default.nix
+++ vars/default.nix
@@ -3,9 +3,15 @@
   userfullname = "章家铭";
   useremail = "zhang_jia_ming@outlook.com";
   initialHashedPassword = "$7$CU..../....vGtVLf5WylKSnJV2R4IaW1$nnsefqHmGiSxV.IIhh71G2VGBixLpYrORADhXAY8CtB";
-  primaryAuthorizedKeys = [ "" ];
+  primaryAuthorizedKeys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIImWd7VyOGb0/yFcBXeKFxDKd6YnoKjroJQvpabZdkOH openpgp@canokey"
+  ];
   secondaryAuthorizedKeys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICgt/2h5OROlOs6L9+SCvSihiq5jcS5TqA+pgeDU9ma5 ming@136kf"
+
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA8Lh4/f3uEk8joZy9pH3NrRmk5rEsFAGJbr9l6BorOI openpgp@136kf"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPvOQM+Ej9fOBgc5IqZaqgBb8/evDR9l4BwCt3dYHu2f openpgp@sgo3"
+
     # e2 TPM 密钥
     "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBBiclJsws+2d6x1hslry7RAf5CkiP8kJNFOQIpiYHdJ2pVPWejC2ZpzlznDclQwwaju0mnG4GS8XgSK9nr0BPr0="
   ];
