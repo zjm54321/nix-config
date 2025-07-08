a6cb79243921f7c28d48f6bda7ce3a86ef933f5b
zjm54321 <zhang_jia_ming@outlook.com>
2025-07-08 15:06:19 +0800

    feat(gpg): setup for wsl

--- host/vm-wsl/home.nix
+++ host/vm-wsl/home.nix
@@ -1,6 +1,20 @@
 {
+  lib,
+  ...
+}:
+{
   imports = [
     ../../home
   ];
   gui.de = "i3";
+
+  # GPG 设备密钥
+  programs.git.signing = lib.mkForce {
+    format = "openpgp";
+    key = "143CA697734657CE";
+  };
+  services.gpg-agent.sshKeys = [
+    # gpg --list-keys --with-keygrip
+    "5A433DADDC09F1A4CC6E10393B50EA54D53F6242"
+  ];
 }
