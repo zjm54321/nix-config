dad3dd60ce3d142139ae0baefa4e3e562f23fa04
zjm54321 <zhang_jia_ming@outlook.com>
2025-07-09 12:12:02 +0800

    fix: wrong commit

--- vars/default.nix
+++ vars/default.nix
@@ -4,7 +4,6 @@
   useremail = "zhang_jia_ming@outlook.com";
   initialHashedPassword = "$7$CU..../....vGtVLf5WylKSnJV2R4IaW1$nnsefqHmGiSxV.IIhh71G2VGBixLpYrORADhXAY8CtB";
   primaryAuthorizedKeys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICgt/2h5OROlOs6L9+SCvSihiq5jcS5TqA+pgeDU9ma5 ming@136kf"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIImWd7VyOGb0/yFcBXeKFxDKd6YnoKjroJQvpabZdkOH openpgp@canokey"
   ];
   secondaryAuthorizedKeys = [
