2fb4fc3f0c1c3f590c93faf59842bf4545b47b1c
zjm54321 <zhang_jia_ming@outlook.com>
2025-08-05 17:13:42 +0800

    feat: update mihomo

--- module/core/network.nix
+++ module/core/network.nix
@@ -1,7 +1,5 @@
 # ----------------------------------------------------------
 # Mihomo 代理配置
-# [todo] 将clash-verge-rev改为mihomo+clashtui
-# https://github.com/JohanChane/clashtui/blob/main/README_ZH.md
 # ----------------------------------------------------------
 {
   config,
@@ -18,22 +16,12 @@ with lib;
   };
 
   config = mkIf config.networking.proxy.mihomo.enable {
-    /*
-      programs.clash-verge = {
-         enable = true;
-         package = pkgs.clash-verge-rev;
-         autoStart = true;
-       };
-    */
     services.mihomo = {
       enable = true;
       tunMode = true;
       webui = pkgs.zashboard;
       configFile = "${inputs.mysecrets}/mihomo/mihomo.yaml";
     };
-    networking.firewall.trustedInterfaces = [
-      "Mihomo"
-      "Meta"
-    ];
+    networking.firewall.trustedInterfaces = [ "Mihomo" ];
   };
 }
