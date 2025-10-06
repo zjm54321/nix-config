d4317aafc84e462843289967180e275dc19d59c0
zjm54321 <zhang_jia_ming@outlook.com>
2025-10-06 21:35:20 +0800

    feat: add mako click action

--- home/gui/desktop/niri/mako.nix
+++ home/gui/desktop/niri/mako.nix
@@ -1,4 +1,9 @@
 {
+  pkgs-unstable,
+  lib,
+  ...
+}:
+{
   services.mako = {
     settings = {
       border-radius = 10;
@@ -6,8 +11,8 @@
       default-timeout = 5000; # 5秒
 
       on-button-left = "invoke-default-action";
-      on-button-right = "dismiss";
-      on-touch = "invoke-default-action";
+      on-button-right = "exec makoctl menu ${lib.getExe pkgs-unstable.rofi} -- -dmenu"; # [fixme] 使用 rofi 2.0 以支持 wayland
+      on-touch = "exec makoctl menu ${lib.getExe pkgs-unstable.rofi} -- -dmenu";
     };
   };
 }
