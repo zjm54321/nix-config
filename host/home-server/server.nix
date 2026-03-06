29d2bcc7c227648e4555b95f9bd04d3205066bcd
zjm54321 <zhang_jia_ming@outlook.com>
2026-03-06 16:49:06 +0800

    fix: disable newapi

--- host/home-server/server.nix
+++ host/home-server/server.nix
@@ -16,7 +16,7 @@
   services.webdav.enable = true;
   services.webdav.settings.directory = "/data/webdav";
   # 启用newapi
-  services.newapi.enable = true;
+  #services.newapi.enable = true;
   # 启用 Aether AI API 网关
   services.aether.enable = true;
 }
