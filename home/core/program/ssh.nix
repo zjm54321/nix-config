8543d6502b28e6c939af22f110e22c2650b97e97
zjm54321 <zhang_jia_ming@outlook.com>
2026-03-15 23:46:22 +0800

    fix: 修复了ssh 权限问题 参考https://github.com/nix-community/home-manager/issues/322#issuecomment-1178614454

--- home/core/program/ssh.nix
+++ home/core/program/ssh.nix
@@ -1,5 +1,6 @@
 {
   vars,
+  lib,
   ...
 }:
 let
@@ -47,5 +48,22 @@ in
 
       "home-server" = host { hostname = "100.100.1.1"; };
     };
+  };
+
+  home.file = {
+    # home-manager wrongly thinks it doesn't manage (and thus shouldn't clobber) this file due to the activation script
+    ".ssh/config".force = true;
+  };
+
+  home.activation = {
+    # https://github.com/nix-community/home-manager/issues/322
+    fixSshPermissions = lib.hm.dag.entryAfter [ "linkGeneration" ] ''
+      run install -d -m 0700 "$HOME/.ssh"
+      if [ -L "$HOME/.ssh/config" ]; then
+        src="$(readlink -f "$HOME/.ssh/config")"
+        run rm -f "$HOME/.ssh/config"
+        run install -m 0600 "$src" "$HOME/.ssh/config"
+      fi
+    '';
   };
 }
