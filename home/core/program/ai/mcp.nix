2b6aac90954105f19b14c6a83238be527a85d71a
zjm54321 <zhang_jia_ming@outlook.com>
2026-03-22 15:33:23 +0800

    feat: 更新 mcp 配置，添加 GitHub 和context7个人访问令牌支持

--- home/core/program/ai/mcp.nix
+++ home/core/program/ai/mcp.nix
@@ -1,7 +1,26 @@
 {
+  secret,
+  ...
+}:
+let
+  githubMcpToken = builtins.elemAt (builtins.match "github.com=(.*)" secret.nix-github-access-tokens) 0;
+in
+{
   mcp-servers.programs = {
-    github.enable = true;
+    github = {
+      enable = true;
+      env = {
+        GITHUB_PERSONAL_ACCESS_TOKEN = githubMcpToken;
+      };
+    };
+
     nixos.enable = true;
-    context7.enable = true;
+
+    context7 = {
+      enable = true;
+      env = {
+        CONTEXT7_API_KEY = secret.context7-api-key;
+      };
+    };
   };
 }
