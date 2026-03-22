d4fc1d485eda3eda42c0c36c007452f9c5f51875
zjm54321 <zhang_jia_ming@outlook.com>
2026-03-22 23:58:40 +0800

    fix: 更新 nushell 配置，添加环境变量支持并修复热加载问题

--- home/core/shell/nushell/default.nix
+++ home/core/shell/nushell/default.nix
@@ -1,7 +1,11 @@
+{ config, ... }:
 {
   programs.nushell = {
     enable = true;
     configFile.source = ./config.nu;
+    # [fixme] 目前 nushell 没有提供热加载配置的方式，所以只能强制替换来确保配置更新
+    # https://github.com/nix-community/home-manager/issues/4313
+    environmentVariables = config.home.sessionVariables;
   };
 
   # 复制 auto-complete 目录到配置目录
