4b263fbda2affe63c46dad906625591478a7045d
zjm54321 <zhang_jia_ming@outlook.com>
2026-04-11 16:02:06 +0800

    add nginx reverse proxy for mihomo webui

--- module/core/network.nix
+++ module/core/network.nix
@@ -1,5 +1,6 @@
 # ----------------------------------------------------------
 # Mihomo 代理配置
+# 访问 http://proxy.localhost/ 即可访问 Mihomo 的 WebUI
 # ----------------------------------------------------------
 {
   config,
@@ -22,6 +23,26 @@ with lib;
       webui = pkgs.metacubexd;
       configFile = "${inputs.mysecrets}/mihomo/mihomo-cloud.yaml";
     };
+
+    services.nginx = {
+      enable = true;
+      virtualHosts."proxy.localhost" = {
+        listen = [
+          {
+            addr = "127.0.0.1";
+            port = 80;
+          }
+        ];
+        locations."= /" = {
+          return = "302 /ui/";
+        };
+        locations."/" = {
+          proxyPass = "http://127.0.0.1:9097";
+          proxyWebsockets = true;
+        };
+      };
+    };
+
     networking.firewall.trustedInterfaces = [ "Mihomo" ];
     systemd.services."mihomo" = {
       after = lib.mkForce [
